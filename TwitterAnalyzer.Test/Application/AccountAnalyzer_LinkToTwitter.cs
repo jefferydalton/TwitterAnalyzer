8d08d1b7cdc15536de9a6b692a055c2257251568
Jeff Dalton <jefferydalton@hotmail.com>
2013-12-30 19:01:13 -0500

    New Unit Test file

---
+++ TwitterAnalyzer.Test/Application/AccountAnalyzer_LinkToTwitter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TwitterAnalyzer.Repository;
+using TwitterAnalyzer.Interfaces;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using TwitterAnalyzer.Application;
+
+namespace TwitterAnalyzer.Test.Application
+{
+    [TestClass]
+    public class AccountAnalyzer_LinkToTwitter
+    {
+
+        private AccountAnalyzer accountApp = new AccountAnalyzer(new AccountQueryLinqToTwitter());
+
+        [TestMethod]
+        public void AccountAnalyzer_IsInstantiated()
+        {
+            Assert.IsInstanceOfType(accountApp, typeof(AccountAnalyzer));
+            Assert.IsInstanceOfType(accountApp.AccountQueryRepository(), typeof(AccountQueryLinqToTwitter));
+        }
+
+        [TestMethod]
+        public void AccountsThatDoNotFollowBack_ReturnsEmptyList()
+        {
+            var response = accountApp.GetFollowingThatAreNotFollowers();
+
+            Assert.AreNotSame(0, response.Count);
+            Assert.IsTrue(response[0].IsFollowing == true && response[0].IsFollower == false);
+        }
+    }
+}
